c439c19f4b17fa8393feb5ccdee068f30aaf39b8
Joe A. Wilde <70703036+JoeAWilde@users.noreply.github.com>
2023-07-05 17:25:57 +0100

    adding missing figures

--- DPmain.cpp
+++ DPmain.cpp
@@ -35,13 +35,13 @@ const double p                   = 0.9; //probability of success on each trial
 // Mating parameters
 const double theta               = 0.25; //parameters controlling the relationship between rivals waving and probability of mating
 const double b                    = 1.0; 
-const double pFemMax              = 0.9;
-const double pFemMin              = 0.001;
+const double pFemMax              = 1.0;
+const double pFemMin              = 0.1;
 const int multi                    = 20;
 
 //Two morphs parameters
-const double q                    = 1.0;
-const double alpha                = 0.0;
+const double q                    = 0.5;
+const double alpha                = 0.75;
 const double zeta                 = 0.0;
 
 /* // pFemMax parameters
@@ -426,10 +426,10 @@ int main()
     {
         for(int t=0; t<tSteps; t++)
         {
-            /* pFemMaxList[tide][t] = pFemMax;
-            pFemMinList[tide][t] = pFemMin; */
+            pFemMaxList[tide][t] = pFemMax;
+            pFemMinList[tide][t] = pFemMin;
 
-            if(t < 60)
+            /* if(t < 60)
             {
                 pFemMinList[tide][t] = pFemMin + 0.0015 * t; 
                 pFemMaxList[tide][t] = pFemMax + 0.0015 * t; 
@@ -440,7 +440,7 @@ int main()
                 pFemMinList[tide][t] = pFemMinList[tide][t-1] -  (0.0015 * 2);
                 pFemMaxList[tide][t] = pFemMaxList[tide][t-1] -  (0.0015 * 2);
 
-            }
+            } */
         }
     }
 
